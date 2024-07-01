7523ecd16bf2cfc18863ded68225a7f0854b4312
f64k <f64k@yandex.ru>
2024-07-01 09:41:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,32 @@
+import os, platform
+import numpy as np, pandas as pd, streamlit as st
 import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.set_page_config(page_title="Предсказания", page_icon="🦋", layout="wide", initial_sidebar_state="expanded")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.sidebar.markdown("💎 Стартовая страница")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+with st.container():
+    cols1 = st.columns([3,3,3])
+    dirParams = {
+        "os.getcwd": os.getcwd(),
+        "cpu_count": os.cpu_count(),
+        #"environ": os.environ,
+        #"os.listdir": os.listdir(),
+        "platform": platform.platform(),
+        "release": platform.release(),
+        "node": platform.node(),
+        "processor": platform.processor(),
+        "machine": platform.machine(),
+        "system": platform.system(),
+        "version": platform.version(),
+        "python_version": platform.python_version(),
+        "python_implementation": platform.python_implementation(),
+        "uname": platform.uname(),
+        "libc_ver": platform.libc_ver(),
+        "architecture": platform.architecture(),
+    }
+    cols1[0].write(dirParams)
+    cols1[1].write(os.environ)
+    cols1[2].write(os.listdir())
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
